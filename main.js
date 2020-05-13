4066c45d434eca6ef065a254f1efc95f1ed10ace
jaesuk <jaesuklee@live.com>
2020-05-13 11:10:16 -0700

    finished contact form mobile and desktop

--- main.js
+++ main.js
@@ -2,6 +2,28 @@
 (function() {
   window.addEventListener("load", init);
 
+  document.addEventListener('DOMContentLoaded', function() {
+    if(typeof(Storage) !== 'undefined') {
+        // See if there is a scroll pos and go there.
+        var lastYPos = +localStorage.getItem('scrollYPos');
+        if (lastYPos) {
+            console.log('Setting scroll pos to:', lastYPos);
+            window.scrollTo(0, lastYPos);
+        }
+
+        // On navigating away first save the position.
+        var anchors = qsa('a');
+
+        var onNavClick = function() {
+            console.log('Saving scroll pos to:', window.scrollY);
+            localStorage.setItem('scrollYPos', window.scrollY);
+        };
+
+        for (var i = 0; i < anchors.length; i++) {
+            anchors[i].addEventListener('click', onNavClick);
+        }
+    }
+  });
 
   function init() { 
     let observer = navObs();
